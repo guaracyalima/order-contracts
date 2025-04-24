93087a1b5215dff7b7dc95f7352b47ff66f3b947
Guaracy A. Lima <guaracyaraujolima@gmail.com>
2025-04-24 00:19:20 -0300

    upgrade and rebuild

--- src/main/java/br/com/zeroth/contracts/OrderRequest.java
+++ src/main/java/br/com/zeroth/contracts/OrderRequest.java
@@ -3,12 +3,14 @@ package br.com.zeroth.contracts;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class OrderRequest {
     private String orderId;
     private String customerId;
