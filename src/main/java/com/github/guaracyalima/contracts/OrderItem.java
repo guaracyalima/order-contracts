69135fe7cf05d89e0c2b84be4cea0d0eb45bed66
Guaracy A. Lima <guaracyaraujolima@gmail.com>
2025-04-24 14:53:11 -0300

    getters and setters

--- src/main/java/com/github/guaracyalima/contracts/OrderItem.java
+++ src/main/java/com/github/guaracyalima/contracts/OrderItem.java
@@ -4,10 +4,40 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 class OrderItem {
     private String productId;
     private Integer quantity;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(String productId, Integer quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "productId='" + productId + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
--- src/main/java/com/github/guaracyalima/contracts/OrderRequest.java
+++ src/main/java/com/github/guaracyalima/contracts/OrderRequest.java
@@ -7,13 +7,62 @@ import lombok.ToString;
 
 import java.util.List;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
+
 public class OrderRequest {
     private String orderId;
     private String customerId;
     private List<OrderItem> items;
     private Double totalAmount;
+
+    public OrderRequest() {
+    }
+
+    public OrderRequest(String orderId, String customerId, List<OrderItem> items, Double totalAmount) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.items = items;
+        this.totalAmount = totalAmount;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderRequest{" +
+                "orderId='" + orderId + '\'' +
+                ", customerId='" + customerId + '\'' +
+                ", items=" + items +
+                ", totalAmount=" + totalAmount +
+                '}';
+    }
 }
